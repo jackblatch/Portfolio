81b6a6fd2dd4b20cd0b7eea0629167bf3be116a6
jackblatch <me@jackblatch.com>
2025-09-02 01:53:41 +0000

    update node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 import mdx from "@astrojs/mdx";
 
